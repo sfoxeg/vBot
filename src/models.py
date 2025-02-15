c36cbe0b4f22c81c8fffba963b0d23f782c34c9a
Sega <sfoxig@ya.ru>
2025-02-15 08:28:40 +0300

    refactor

--- src/models.py
+++ src/models.py
@@ -1,14 +1,14 @@
-import datetime
+from datetime import datetime
 from typing import Annotated
 from sqlalchemy import text
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
 intpk = Annotated[int, mapped_column(primary_key=True)]
-created_at = Annotated[datetime.datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
-updated_at = Annotated[datetime.datetime, mapped_column(
+created_at = Annotated[datetime, mapped_column(server_default=text("TIMEZONE('utc', now())"))]
+updated_at = Annotated[datetime, mapped_column(
     server_default=text("TIMEZONE('utc', now())"),
-    onupdate=datetime.datetime.utcnow)]
+    onupdate=datetime.utcnow)]
 
 
 class UsersOrm(Base):
